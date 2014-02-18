78251da586294c927e1fa788491246ead53213a0
Mark Shropshire <marks@knowclassic.com>
2014-02-18 07:41:25 -0500

    We can now delete lists!

--- client/views/lists.js
+++ client/views/lists.js
@@ -1,3 +1,13 @@
 Template.lists.lists = function () {
   return Lists.find({});
 };
+
+Template.lists.events({
+  'click .delete-list': function(e) {
+    e.preventDefault();
+    
+    if (confirm("Delete list?")) {
+      Lists.remove(this._id);
+    }
+  }
+});
