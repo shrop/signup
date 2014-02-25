e3e28640dbc9bb12454cf0a0e23f28e8c071faa7
Scott Deeter <scottd@knowclassic.com>
2014-02-24 20:10:15 -0500

    Added ability to submit list titles

--- client/views/lists.js
+++ client/views/lists.js
@@ -5,9 +5,14 @@ Template.lists.lists = function () {
 Template.lists.events({
   'click .delete-list': function(e) {
     e.preventDefault();
-    
+
     if (confirm("Delete list?")) {
       Lists.remove(this._id);
     }
+  },
+  'click .add-list': function() {
+    var new_list_name = document.getElementById('title').value;
+    Lists.insert({title: new_list_name});
+    document.getElementById('title').value = null;
   }
 });
